da3c388a3e472e4ca67df6a943a50c5268adb4df
Dima Yakushuk <yakushyk.dmitrii@gmail.com>
2021-09-12 13:31:16 +0300

    delete @Data

--- src/main/java/models/Address.java
+++ src/main/java/models/Address.java
@@ -2,13 +2,13 @@ package models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
 
-@Data
 @AllArgsConstructor
-@ToString
+@NoArgsConstructor
 @Entity
 @Table(name = "user_address")
 public class Address {
@@ -26,7 +26,47 @@ public class Address {
     @Column(name = "house")
     private String house;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL)  // todo что за каскад ( каскад удаляет из привязанной таблицы при удалении юзера)
     @JoinColumn (name = "address_id")
     private User user;
 
@@ -34,8 +74,5 @@ public class Address {
         this.city = city;
         this.street = street;
         this.house = house;
-    }
-
-    public Address() {
     }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -2,14 +2,14 @@ package models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
 
-@Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@ToString
 @Table(name = "users")
 public class User {
 
@@ -27,7 +27,47 @@ public class User {
     @Column (name = "age")
     private int age;
 
-    @OneToOne(mappedBy = "user", orphanRemoval = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @OneToOne(mappedBy = "user", orphanRemoval = true)  //если адрес удалили то и с базы он удалиться
     private Address address;
 
     public User(String firstName, String lastName, int age) {
@@ -42,8 +82,4 @@ public class User {
         this.lastName = lastName;
         this.age = age;
     }
-
-    public User() {
-    }
-
 }
